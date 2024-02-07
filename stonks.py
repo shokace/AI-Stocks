5f3e51e3c94a247a05d3832188e2f1715d0f7c0d
Petar Juric <petarjuricbepo@yahoo.com>
2024-02-07 15:21:11 -0800

    split handler into different task

--- stonks.py
+++ stonks.py
@@ -58,37 +58,6 @@ def index():
     return render_template('index.html', graph_html=fig)
 
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    # Validate the request
-    print("WEBHOOK'D")
-    signature = request.headers.get('X-Hub-Signature')
-    sha_name, signature = signature.split('=')
-    print(sha_name, signature)
-    if sha_name != 'sha1':
-        abort(501)
-
-    # HMAC requires the key to be bytes, but data is string
-    mac = hmac.new(GITHUB_SECRET_BYTES, msg=request.data, digestmod=hashlib.sha1)
-
-    # Verify the signature
-    if not hmac.compare_digest(str(mac.hexdigest()), str(signature)):
-        abort(403)
-
-    def deployment():
-        # If the signature is valid, run deployment
-        try:
-            subprocess.call(['/bin/bash', f'{REPO_PATH}/../deploy.sh'])
-            print("DONE")
-        except subprocess.CalledProcessError as e:
-            print(f"Error: {e}")  
-
-    thread = Thread(target=deployment)
-    thread.start()
-
-    return 'OK', 200
-
-
 
 if __name__ == "__main__":
     app.run(debug=False, host="127.0.0.1", port=8000)
