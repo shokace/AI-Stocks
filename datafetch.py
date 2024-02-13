453d82295c210c9a2a4eae6042e3c61e472ae0ff
Petar Juric <petarjuricbepo@yahoo.com>
2024-02-12 22:27:56 -0800

    Threading Process: Top Mover Graph

--- datafetch.py
+++ datafetch.py
@@ -1,7 +1,10 @@
+from stonks import app
+from dotenv import load_dotenv
+
 import requests
 import time
 import os
-from dotenv import load_dotenv
+
 
 
 
@@ -10,40 +13,40 @@ current_unix_time = int(time.time())
 yesterday_unix_time = current_unix_time - 86400
 load_dotenv()
 
-def getLastTime():
-    return current_unix_time
-
 def fetch_granular_data(crypto_name):
-    base_url = "https://api.coingecko.com/api/v3/coins/{}/market_chart/range?vs_currency=usd&from={}&to={}&precision=8&x_cg_demo_api_key={}"   
-    coingecko_token = os.getenv('COINGECKO_KEY')
-    url = base_url.format(crypto_name, yesterday_unix_time, current_unix_time, coingecko_token)
-    print(url)
+    with app.app_context():
+        base_url = "https://api.coingecko.com/api/v3/coins/{}/market_chart/range?vs_currency=usd&from={}&to={}&precision=8&x_cg_demo_api_key={}"   
+        coingecko_token = os.getenv('COINGECKO_KEY')
+        url = base_url.format(crypto_name, yesterday_unix_time, current_unix_time, coingecko_token)
+        print(url)
 
-    response = requests.get(url)
-    if response.status_code == 200:
-        print("COINGECKO API = 200")
-        return response.json()
-    else:
-        # Handle errors (e.g., invalid cryptocurrency name, network issues)
-        return None
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("COINGECKO API = 200")
+            return response.json()
+        else:
+            # Handle errors (e.g., invalid cryptocurrency name, network issues)
+            return None
 
 
 
 def fetch_crypto_data(tm_url):
-    response = requests.get(tm_url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    with app.app_context():
+        response = requests.get(tm_url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
 
 
 def top_mover(crypto_data):
-    max_change = 0
-    top_crypto = None
-    for crypto in crypto_data:
-        price_change_percentage_24h = crypto.get('price_change_percentage_24h', 0)
-        if price_change_percentage_24h > max_change:
-            max_change = price_change_percentage_24h
-            top_crypto = crypto
-    return top_crypto
+    with app.app_context():
+        max_change = 0
+        top_crypto = None
+        for crypto in crypto_data:
+            price_change_percentage_24h = crypto.get('price_change_percentage_24h', 0)
+            if price_change_percentage_24h > max_change:
+                max_change = price_change_percentage_24h
+                top_crypto = crypto
+        return top_crypto
 
--- stonks.py
+++ stonks.py
@@ -1,17 +1,53 @@
-from flask import Flask, render_template, request, abort
-import schedule
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+
+import threading
 import time
 import datetime as datetime
 import datafetch
 import dataplot
-from dotenv import load_dotenv
 import os
 
-
+load_dotenv()
 
 
 
 app = Flask(__name__)
+
+def periodic_fetch():
+    while True:
+        
+        tm_url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&x_cg_demo_api_key={}"
+        coingecko_token = os.getenv('COINGECKO_KEY')
+        url = tm_url.format(coingecko_token)
+        # Fetch data        
+        try:
+            crypto_data = datafetch.fetch_crypto_data(url)
+            top_crypto = datafetch.top_mover(crypto_data)
+            print(top_crypto)
+        except Exception as _:
+            print("Base data could not be fetched.")
+
+        
+        # Find the top mover
+        if crypto_data:
+            if top_crypto:
+                crypto_name = top_crypto['id']
+                #crypto_name = 'bitcoin'
+                
+                granular_data = datafetch.fetch_granular_data(crypto_name)
+                
+                #print(granular_data)
+                dataplot.plotdata(crypto_name, granular_data)
+                print("GRAPH UPDATED")
+            else:
+                print("No top mover found.")
+        else:
+            print("Failed to fetch data.")
+        
+        time.sleep(600)  # Wait for 10 minutes
+
 
 #init render template
 @app.route('/')
@@ -22,44 +58,14 @@ def index():
         defaultFig = file.read()
         file.close()
 
-    print("file was opened")
-    print(int(time.time()) - datafetch.current_unix_time)
-    if (int(time.time()) - datafetch.current_unix_time >= 600):
-        
-        tm_url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
 
-        # Fetch data
-        try:
-            crypto_data = datafetch.fetch_crypto_data(tm_url)
-            top_crypto = datafetch.top_mover(crypto_data)
-            print(top_crypto)
-        except Exception as _:
-            print("Base data could not be fetched.")
-
-
-        # Find the top mover
-        if crypto_data:
-            if top_crypto:
-                crypto_name = top_crypto['id']
-                #crypto_name = 'bitcoin'
-                
-                granular_data = datafetch.fetch_granular_data(crypto_name)
-                
-                #print(granular_data)
-                fig = dataplot.plotdata(crypto_name, granular_data)
-                print("FIG READY")
-                return render_template('index.html', graph_html=fig)
-            else:
-                print("No top mover found.")
-        else:
-            print("Failed to fetch data.")
-
-        
-        
     return render_template('index.html', graph_html=defaultFig)
 
 
 if __name__ == "__main__":
+    t = threading.Thread(target=periodic_fetch)
+    t.daemon = True  # This makes the thread terminate when the main program exits
+    t.start()  # Start the thread
     app.run(debug=False, host="127.0.0.1", port=8000)
 
     
