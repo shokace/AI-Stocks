4a500de2b58bc647cff2d45af2ef5435918f8dbd
Petar Juric <petarjuricbepo@yahoo.com>
2024-02-24 00:14:51 -0800

    init testbed for youtube API calls

---
+++ ML/dataCollector.py
@@ -0,0 +1,13 @@
+import os
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+import dotenv
+
+scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+
+def ytCollect(): #we're gonna start collecting yt video comments :) AND THEN ANALYZE THEM >:)
+    return 0
+
+if __name__ == "__main__":
+    ytCollect()
